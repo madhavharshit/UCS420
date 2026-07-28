275a049f21aae1dfc864293441dfb397cc203f6f
madhavharshitcodes <pachaurih81@gmail.com>
2026-07-28 09:07:47 +0530

    assign4

---
+++ assign4.py
@@ -0,0 +1,15 @@
+#assign 4.1
+for i in range(1,11):
+    print(i*7)
+for i in range(1,11):
+    print(i*9)
+
+#assign 4.2
+n = int(input("Enter a number: "))
+for i in range(1,11):
+    print(i*n)
+
+sum = 0
+for i in range(1,n+1):
+    sum += i
+print(sum)
